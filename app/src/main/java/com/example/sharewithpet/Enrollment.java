5ffac1d58a08a98df44d9a0574c8531ebe77f569
bell0139b@gmail.com <bell0139b@gmail.com>
2021-12-03 05:33:13 +0900

    GUI

---
+++ app/src/main/java/com/example/sharewithpet/Enrollment.java
@@ -0,0 +1,14 @@
+package com.example.sharewithpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Enrollment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enrollment);
+    }
+}
